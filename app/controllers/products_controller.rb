2a7ea836a9db7842a179035adbc006e5a6a7a66d
AlisG26 <apgb1996@gmail.com>
2018-09-24 20:42:44 -0500

    siguiente commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,8 +3,40 @@ class ProductsController < ApplicationController
         @products = Product.all
     end
 
-    def mew
-        @products = Product.new
+    def new
+        @product = Product.new
     end
+
+    def create
+        @product = Product.new(product_params)
+          if @product.save
+            redirect_to products_path
+          else
+            render :new
+          end
+    end
+
+    def edit
+        @product = Product.find(params[:id])
+      end
+
+    def update
+        @product = Product.find(params[:id])
+        if @product.update(product_params)
+            redirect_to products_path
+        else
+            render :edit
+        end
+    end
+    
+    
+    private
+    def product_params
+        params.require(:product).permit(:name, :description, :price)
+    end
+    
+  
+    
+    
     
 end
